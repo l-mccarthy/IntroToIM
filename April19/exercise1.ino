af154fcb8b63e06ce37ff427c6b87379469f9fde
l-mccarthy <98512628+l-mccarthy@users.noreply.github.com>
2022-04-18 11:53:00 +0400

    Create exercise1.ino

---
+++ April19/exercise1.ino
@@ -0,0 +1,14 @@
+////////////////////////////////////////
+// Moving Ellipse Using Potentiometer //
+////////////////////////////////////////
+
+void setup() {
+  Serial.begin(9600);     // initiate the serial monitor
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);                 // read the value from A0 pin 
+  int mapped = map(sensorValue, 0, 1023, 0, 400);   // map the values between the potentiometer and 
+                                                    // the screen width and height in p5 (400 is chosen in p5)
+  Serial.println(mapped);                           // print the number on Serial monitor
+}
